e407386e6ee8bcc6a8019bbf4b5fa0394f796bc4
Maxwell Boykin <maxericboykin@gmail.com>
2021-09-30 16:07:53 -0400

    css

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const morgan = require("morgan");
+// const layout = require("./views/layout");
+const views = require("./views/");
 
 
 const app = express();
 app.use(morgan("dev"));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/")); //style.css for our homepage
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
   console.log('i am working!');
-  res.send("hello world");
+  res.send(views());
   res.status(200);
 })
 
-const PORT = 8081;
+const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
